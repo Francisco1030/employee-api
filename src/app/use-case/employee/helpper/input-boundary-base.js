80fda90b297e3891e9e22c3f093e43dd05cbb243
Francisco Viana <franciscoviana862@gmail.com>
2021-07-12 21:51:03 -0300

    feat: add helpper InputBoundaryBase and OutputBoundaryBase;

---
+++ src/app/use-case/employee/helpper/input-boundary-base.js
@@ -0,0 +1,8 @@
+module.exports = class InputBoundaryBase {
+  constructor(input) {
+    this.id = input.id;
+    this.age = input.age;
+    this.name = input.name;
+    this.office = input.office;
+  }
+}
---
+++ src/app/use-case/employee/helpper/output-boundary-base.js
@@ -0,0 +1,8 @@
+module.exports = class OutputBoundaryBase {
+  constructor(output) {
+    this.id = output.id;
+    this.age = output.age;
+    this.name = output.name;
+    this.office = output.office;
+  }
+}
